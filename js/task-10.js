fcbe96f5ee36af88d32c0d0aee617ae803a0f304
Tanya <tkravche@yahoo.com>
2023-03-13 22:19:42 +0100

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,41 +1,32 @@
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает
-// кнопку Создать, после чего рендерится коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
-
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>,
-//сколько указано в amount и добавляет их в div#boxes.
-
-//     Размеры самого первого <div> - 30px на 30px.
-//     Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-//     Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor
-// для получения цвета.
-
-
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-const amount = 2;
-const boxesDiv = document.querySelector('#boxes')
-function createBoxes(amount){
-  const createdElement = document.createElement('div');
-  console.log(createdElement.style)
 
-  createdElement.style.height = '30px';
-  createdElement.style.width = '30px';
-  createdElement.style.backgroundColor = getRandomHexColor();
-  boxesDiv.append(createdElement)
-  console.log(createdElement.style)
+const amountInput = document.querySelector("input");
+const boxesDiv = document.querySelector("#boxes");
+
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+
+createButton.addEventListener("click", () => createBoxes(amountInput.value));
+
+let boxWidth = 20;
+let boxHeight = 20;
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    const createdElement = document.createElement("div");
+    boxWidth += 10;
+    boxHeight += 10;
+    createdElement.style.height = `${boxWidth}px`;
+    createdElement.style.width = `${boxHeight}px`;
+    createdElement.style.backgroundColor = getRandomHexColor();
+    boxesDiv.append(createdElement);
+  }
 }
-  
-console.log(createBoxes(2))
+
+destroyButton.addEventListener("click", destroyBoxes);
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
